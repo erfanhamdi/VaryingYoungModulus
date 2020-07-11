7ce6689d9d49d437b8874f0e64c5f5e21b840667
ErfanHamdi <erfan.hamdi@gmail.com>
2020-07-11 13:51:40 +0430

    the working script

--- Evaries.py
+++ Evaries.py
@@ -11,21 +11,21 @@ import sketch
 import partition
 
 beamProfileSketch = beamModel.ConstrainedSketch(name = 'Beam CS Profile', sheetSize = 5)
-beamProfileSketch.rectangle(point1 = (0.1,0.1), point2 = (0.3, -0.1)
+beamProfileSketch.rectangle(point1 = (0.1,0.1), point2 = (0.3, -0.1))
 
-beamPart = beamModel.Part(name = 'Beam', dimensionality = THREE_D, type = DEFORMABLE_BODY)
+beamPart = beamModel.Part(name = 'Beam', dimensionality = THREE_D, type=DEFORMABLE_BODY)
 beamPart.BaseSolidExtrude(sketch = beamProfileSketch, depth = 5)
 
 # Creating Material
 import material
 
 beamMaterial = beamModel.Material(name = 'AISI 1005 Steel')
-beamMaterial.Density(tabel = ((7872, ),))
+beamMaterial.Density(table = ((7872, ),))
 N = 0
 E_c = 380e9
 E_m = 70e9
-x = np.linspace(0,a,100)
-E_var = E_m+(E_c-E_m)*(x/a)**n 
+#x = np.linspace(0,a,100)
+#E_var = E_m+(E_c-E_m)*(x/a)**n 
 beamMaterial.Elastic(table = ((200e9, 0.29),))
 
 # Creating Sections
@@ -55,12 +55,12 @@ del beamModel.historyOutputRequests['H-Output-1']
 # Applying the Load
 top_face_pt_x = 0.2
 top_face_pt_y = 0.1
-top_face_pt_z = 2. 5
+top_face_pt_z = 2.5
 top_face_pt = (top_face_pt_x, top_face_pt_y, top_face_pt_z)
 # The face on which that point lies is the face we are looking for
-top_face = beamlnstance.faces.findAt((top_face_pt,))
+top_face = beamInstance.faces.findAt((top_face_pt,))
 # We extract the region of the face choosing which direction its normal points in
-top_face_region=regionToolset.Region(sidelFaces=top_face)
+top_face_region=regionToolset.Region(side1Faces=top_face)
 #Apply the pressure load on this region in the 'Apply Load' step
 beamModel.Pressure(name='Uniform Applied Pressure', createStepName='Apply Load', region=top_face_region, distributionType=UNIFORM, magnitude=10, amplitude=UNSET) 
 
@@ -73,7 +73,7 @@ fixed_end_face_pt_x = 0.2
 fixed_end_face_pt_y = 0
 fixed_end_face_pt_z = 0
 fixed_end_face_pt = (fixed_end_face_pt_x, fixed_end_face_pt_y, fixed_end_face_pt_z) 
-fixed_end_face = beaminstance.faces.findAt((fixed_end_face_pt,))
+fixed_end_face = beamInstance.faces.findAt((fixed_end_face_pt,))
  # we extract the region of the face choosing which direction its normal points in
 fixed_end_face_region=regionToolset.Region(faces=fixed_end_face)
 beamModel.EncastreBC(name='Encaster one end', createStepName='Initial', region=fixed_end_face_region)
@@ -89,24 +89,24 @@ beamCells=beamPart.cells
 selectedBeamCells=beamCells.findAt((beam_inside_xcoord, beam_inside_ycoord, beam_inside_zcoord),)
 beamMeshRegion=(selectedBeamCells,)
 beamPart.setElementType(regions=beamMeshRegion, elemTypes=(elemType1,))
-beamPart.seedPart(size=0.l, deviationFactor=0.l)
+beamPart.seedPart(size=0.1, deviationFactor=0.1)
 beamPart.generateMesh()
 
 # Creating and Running the Job
 import job
 
-mdb.Job(name='CantileverBeamJob', model='Cantilever Beam', type=ANALYSIS,
+mdb.Job(name='CantileverBeamJob2', model='Cantilever Beam', type=ANALYSIS,
 	explicitPrecision=SINGLE, nodalOutputPrecision=SINGLE, 
 	description='Job simulates a loaded cantilever beam', 
 	parallelizationMethodExplicit=DOMAIN, 
 	multiprocessingMode=DEFAULT, 
-	numDomains=1, userSubroutine=' ', numCpus=1, memory=50,
+	numDomains=1, userSubroutine='', numCpus=1, memory=50,
 	memoryUnits=PERCENTAGE, scratch='', echoPrint=OFF, 
 	modelPrint=OFF, contactPrint=OFF, historyPrint=OFF)
 
 # Run the job
-mdb.jobs['CantileverBeamJob').submit(consistencyChecking=OFF)
+mdb.jobs['CantileverBeamJob2'].submit(consistencyChecking=OFF)
 # Do not return control till job is finished running
-mdb.jobs('CantileverBeamJob'].waitForCompletion()
+mdb.jobs['CantileverBeamJob'].waitForCompletion()
 # End of run job 
 
